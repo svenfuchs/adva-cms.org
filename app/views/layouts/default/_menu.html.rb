dda4d41b7697a8b486b3e9b3fac1b7ee0787e80c
chris flöß <skeptikos@gmail.com>
2010-11-08 13:52:02 +0100

    Add blog entry for adva-cms2 release 0.0.2

--- app/views/layouts/default/_menu.html.rb
+++ app/views/layouts/default/_menu.html.rb
@@ -1,12 +1,19 @@
 class Layouts::Default::Menu < Adva::View::Menu
   include do
-    def to_html
+    def build
       ul(:id => :menu) do
         site.sections.roots.each do |section|
           item(section.name, url_for(section))
         end
+        render_items
       end
+    end
+
+    protected
+
+    def active?(url, options)
+      return false if url =~ %r(/admin/sites(/\d+)?$) && request.fullpath != url
+      super
     end
   end
 end
-
